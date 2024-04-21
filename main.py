4723dc61c5a5b0a442032de286b82a44436bab8c
dsyouness <y.drissislimani@gmail.com>
2024-04-22 01:28:48 +0200

    (fix) change Dockerfile

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+
 from fastapi import FastAPI
 import subprocess
 
@@ -9,4 +11,14 @@ def run_dbt():
     result = subprocess.run(["dbt", "run"], capture_output=True, text=True)
 
     # Return the output as a response
+    return {"output": result.stdout, "error": result.stderr}
+
+@app.get("/docs")
+def expose_docs():
+    # log the output of the command
+    result = subprocess.run(["dbt", "docs", "generate"], capture_output=True, text=True)
+    logging.info(result.stdout)
+    result = subprocess.run(["dbt", "docs", "serve"], capture_output=True, text=True)
+    logging.info(result.stdout)
+
     return {"output": result.stdout, "error": result.stderr}
